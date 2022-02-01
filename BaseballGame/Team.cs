faf08c697c9a28de028a20c036a691ef9e73df15
Tim Potter <potter.dev23@gmail.com>
2022-02-01 01:18:00 -0500

    redo Team class

--- BaseballGame/Team.cs
+++ BaseballGame/Team.cs
@@ -7,17 +7,7 @@ namespace BaseballLeague
 	public class Team
 	{
 		private string _name;
-		public string Name { 
-			set 
-			{
-				_name = value;
-			}
-			get
-			{
-				return _name;
-			} 
-		}
-
+		public string Name { set { _name = value; } get { return _name; } }
 		private List<Coach> _coaches;
 		private List<Player> _players;
 
@@ -25,29 +15,28 @@ namespace BaseballLeague
 
 		// Designated Constructor
 		public Team(string name)
-		{
+        {
 			Name = name;
 			_coaches = new List<Coach>();
-			_players = new List<Player>();
-		}
+			_players = new List<Player>();	
+        }
 
 		public bool Add(Coach coach)
-		{
+        {
 			bool success = false;
 			_coaches.Add(coach);
 			success = true;
 
 			return success;
-		}
+        }
 
 		public bool Add(Player player)
-		{
+        {
 			bool success = false;
 			_players.Add(player);
 			success = true;
 
 			return success;
-		}
-
+        }
 	}
 }
