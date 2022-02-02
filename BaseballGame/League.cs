96e4e375d5967d8612aa1ef59f2127ca13630d70
Tim Potter <potter.dev23@gmail.com>
2022-02-01 23:21:19 -0500

    Tabs

--- BaseballGame/League.cs
+++ BaseballGame/League.cs
@@ -15,41 +15,41 @@ namespace BaseballLeague
 
 		// Designated Constructor
 		public League(string name)
-        {
+		{
 			Name = name;
 			_players = new List<Player>();
 			_coaches = new List<Coach>();
 			_teams = new List<Team>();
-        }
+		}
 
 		public bool CreatePlayer(string lastName, string firstName)
-        {
+		{
 			bool success = false;
 			Player player = new Player(lastName, firstName);
 			_players.Add(player);
 			success = true;
 
 			return success;
-        }
+		}
 
 		public bool CreateCoach(string lastName, string firstName)
-        {
+		{
 			bool success = false;
 			Coach coach = new Coach(lastName, firstName);
 			_coaches.Add(coach);
 			success = true;
 
 			return success;
-        }
+		}
 
 		public bool CreateTeam(string name)
-        {
+		{
 			bool success = false;
 			Team team = new Team(name);
 			_teams.Add(team);
 			success = true;
 
 			return success;
-        }
+		}
 	}
 }
