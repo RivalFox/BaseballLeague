d15d5d4c5c3bbf7d3442f4bfc91dc05ba028eb8a
Tim Potter <potter.dev23@gmail.com>
2022-02-17 11:35:31 -0500

    Add CreatePlayerCommand.cs

---
+++ BaseballGame/CreatePlayerCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseballLeague
+{
+    public class CreatePlayerCommand : Command
+    {
+        public CreatePlayerCommand() : base("CreatePlayer")
+        {
+
+        }
+        override
+        public bool Execute(League league)
+        {
+            bool success = false;
+            if(Param0 != null && Param1 != null)
+            {
+                success = league.CreatePlayer(Param0, Param1);
+            }
+            else
+            {
+                Console.WriteLine("There was an error executing the Create Player command.");
+            }
+            return success;
+        }
+    }
+}
