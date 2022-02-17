cd58d4e8114f1fd626ce9e72e6b980aaf69a411f
Tim Potter <potter.dev23@gmail.com>
2022-02-17 11:28:29 -0500

    Add Command.cs

---
+++ BaseballGame/Command.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseballLeague
+{
+    public abstract class Command
+    {
+        public string Name { set; get; }
+        public string Param0 { set; get; }
+        public string Param1 { set; get; }
+        public string Param2 { set; get; }
+        public abstract bool Execute(League league);
+
+        public Command() : this("Nameless")
+        {
+        }
+
+        // Designated Contructor
+        public Command(string name)
+        {
+            Name = name;
+            Param0 = null;
+            Param1 = null;
+            Param2 = null;
+        }
+    }
+}
