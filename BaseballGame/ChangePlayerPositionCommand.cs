b8eee67cdf42bc87d9ad6b0d2273399774e07d23
Tim Potter <potter.dev23@gmail.com>
2022-02-18 05:21:27 -0500

    add changeplayerpositionCommand.cs

---
+++ BaseballGame/ChangePlayerPositionCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseballLeague
+{
+    public class ChangePlayerPositionCommand : Command
+    {
+        public ChangePlayerPositionCommand() : base("ChangePlayerPosition") { }
+
+        override
+        public bool Execute(League league)
+        {
+            bool success = false;
+            if(Param0 != null && Param1 != null && Param2 != null)
+            {
+                success = league.ChangePositionToPlayer(Param0, Param1, Param2);
+            }
+            else
+            {
+                Console.WriteLine("There is input missing to change the position of a player");
+            }
+            return success;
+        }
+    }
+}
