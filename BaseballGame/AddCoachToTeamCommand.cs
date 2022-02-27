1df4086eeb711a7614647a7d5a3fddf429062033
Tim Potter <potter.dev23@gmail.com>
2022-02-27 03:59:27 -0500

    Created Add CoachToTeamCommand and PlayerToTeamCommand

---
+++ BaseballGame/AddCoachToTeamCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseballLeague
+{
+    public class AddCoachToTeamCommand : Command
+    {
+        public AddCoachToTeamCommand() : base("AddCoachToTeam") { }
+
+        override
+        public bool Execute(League league)
+        {
+            bool success = false;
+            if(Param0 != null && Param1 != null && Param2 != null)
+            {
+                success = league.AddCoachToTeam(Param0, Param1, Param2);
+            }
+            else
+            {
+                Console.WriteLine("There is input missing to add a Coach to a Team");
+            }
+            return success;
+        }
+
+    }
+}
---
+++ BaseballGame/AddPlayerToTeamCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseballLeague
+{
+    public class AddPlayerToTeamCommand : Command
+    {
+        public AddPlayerToTeamCommand() : base("AddPlayerToTeam") { }
+
+        override
+        public bool Execute(League league)
+        {
+            bool success = false;
+            if (Param0 != null && Param1 != null && Param2 != null)
+            {
+                success = league.AddPlayerToTeam(Param0, Param1, Param2);
+            }
+            else
+            {
+                Console.WriteLine("There is input missing to add a Player to a Team");
+            }
+            return success;
+        }
+    }
+}
