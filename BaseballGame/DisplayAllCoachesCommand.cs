90758c026408426d7607303b11e62d0ba06a7e16
Tim Potter <potter.dev23@gmail.com>
2022-02-27 03:27:07 -0500

    Added DisplayAllCoachesCommand.cs

---
+++ BaseballGame/DisplayAllCoachesCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseballLeague
+{
+    public class DisplayAllCoachesCommand : Command
+    {
+        public DisplayAllCoachesCommand() : base("DisplayAllCoaches") { }
+
+        override
+        public bool Execute(League league)
+        {
+            bool success = true;
+            league.DisplayListOfAllCoaches();
+
+            return success;
+        }
+    }
+}
