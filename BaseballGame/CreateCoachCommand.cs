3ad12ba5476720f85dbf6f45ef8db55b5444c5d9
Tim Potter <potter.dev23@gmail.com>
2022-02-27 03:16:03 -0500

    Added CreateCoachCommand.cs

---
+++ BaseballGame/CreateCoachCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseballLeague
+{
+    public class CreateCoachCommand : Command
+    {
+        public CreateCoachCommand() : base("CreatePlayer") { }
+
+        override
+        public bool Execute(League league)
+        {
+            bool success = false;
+            if(Param0 != null && Param1 != null)
+            {
+                success = league.CreateCoach(Param0, Param1);
+            }
+            else
+            {
+                Console.WriteLine("There was an error executing the Create Coach Command");
+            }
+            return success;
+        }
+    }
+}
