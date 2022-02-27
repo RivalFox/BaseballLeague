9022a3494f694bbdf8f5b25424ebf3e695673f06
Tim Potter <potter.dev23@gmail.com>
2022-02-27 03:30:33 -0500

    Added DisplayAllTeamsCommand.cs

---
+++ BaseballGame/DisplayAllTeamsCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseballLeague
+{
+    public class DisplayAllTeamsCommand : Command
+    {
+        public DisplayAllTeamsCommand() : base("DisplayAllTeams") { }
+
+        override
+        public bool Execute(League league)
+        {
+            bool success = true;
+            league.DisplayListOfAllTeams();
+
+            return success;
+        }
+    }
+}
