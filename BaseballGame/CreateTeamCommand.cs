6865f23fe63cdccbab80dafb5f01218d285efc29
Tim Potter <potter.dev23@gmail.com>
2022-02-27 03:21:10 -0500

    Added CreateTeamCommand.cs

---
+++ BaseballGame/CreateTeamCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseballLeague
+{
+    public class CreateTeamCommand : Command
+    {
+        public CreateTeamCommand() : base("CreateTeam") { }
+
+        override
+        public bool Execute(League league)
+        {
+            bool success = false;
+            if(Param0 != null)
+            {
+                success = league.CreateTeam(Param0);
+            }
+            else
+            {
+                Console.WriteLine("There was an error executing Create Team Command");
+            }
+            return success;
+        }
+    }
+}
