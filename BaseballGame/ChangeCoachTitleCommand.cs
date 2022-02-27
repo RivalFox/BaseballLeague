5fd51d502055ae3e90d5e0aa9fc0e6fd533f0b87
Tim Potter <potter.dev23@gmail.com>
2022-02-27 03:45:32 -0500

    Added ChangeCoachTitleCommand.cs

---
+++ BaseballGame/ChangeCoachTitleCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseballLeague
+{
+    public class ChangeCoachTitleCommand : Command
+    {
+        public ChangeCoachTitleCommand() : base("ChangeCoachTitle") { }
+
+        override
+        public bool Execute(League league)
+        {
+            bool success = false;
+            if(Param0 != null && Param1 != null && Param2 != null)
+            {
+                success = league.ChangeTitleToCoaches(Param0, Param1, Param2);
+            }
+            else
+            {
+                Console.WriteLine("There is input missing to change Coach Title");
+            }
+
+            return success;
+        }
+    }
+}
