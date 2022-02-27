d49e6cde9db003bc4503653851c1ae8329351bc0
Tim Potter <potter.dev23@gmail.com>
2022-02-27 03:38:39 -0500

    Added DisplayTeamRosterCommand.cs

---
+++ BaseballGame/DisplayTeamRosterCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseballLeague
+{
+    public class DisplayTeamRosterCommand : Command
+    {
+        public DisplayTeamRosterCommand() : base("DisplayTeamRoster") { }
+
+        override
+        public bool Execute(League league)
+        {
+            bool success = true;
+            if(Param0 != null)
+            {
+                league.DisplayTeamRoster(Param0);
+                success = true;
+            }
+
+            return success;
+        }
+    }
+}
